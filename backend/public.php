6d00b9885905cfd491e7d670f55f21d0f0b9d485
andres <andres.delgado@cloudframework.io>
2024-05-26 00:57:22 +0200

    feat: added modularity to endpoint

--- backend/public.php
+++ backend/public.php
@@ -77,53 +77,7 @@ if(basename($_SERVER['REQUEST_URI'])==='traduccion'){
                 throw new Exception("Número máximo de traducciones mensual alcanzaddo");
             }else{
                 // Llamada Api al software de traducción
-                // Datos para la solicitud
-                $apiKey = "bd7210a9-bcd9-df24-d8b8-01aec4676138:fx";
-
-                // Configuración de la solicitud cURL
-                $url = "https://api-free.deepl.com/v2/translate";
-                if($idiomaEntrada!='default'){
-                    $data = array(
-                        'text' => $textoEntrada,
-                        'target_lang' => $idiomaSalida,
-                        'source_lang' => $idiomaEntrada,
-                        'formality' => $formal?'prefer_more':'default',
-                        'preserve_formatting' => $formato
-                    );
-                }else{
-                    $data = array(
-                        'text' => $textoEntrada,
-                        'target_lang' => $idiomaSalida,
-                        'formality' => $formal?'prefer_more':'default',
-                        'preserve_formatting' => $formato
-                    );
-                }  
-
-                $options = array(
-                    CURLOPT_URL => $url,
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_POST => true,
-                    CURLOPT_POSTFIELDS => http_build_query($data),
-                    CURLOPT_HTTPHEADER => array(
-                        'Authorization: DeepL-Auth-Key ' . $apiKey,
-                        'Content-Type: application/x-www-form-urlencoded',
-                    ),
-                );
-
-                $ch = curl_init();
-                curl_setopt_array($ch, $options);
-
-                // Ejecutar la solicitud cURL y obtener la respuesta
-                $response = json_decode(curl_exec($ch),true);
-                $textoTraducido = $response['translations'][0]['text'];
-
-                // Verificar si hubo algún error
-                if (curl_errno($ch)) {
-                    echo 'Error en la solicitud cURL: ' . curl_error($ch);
-                }
-
-                // Cerrar la sesión cURL
-                curl_close($ch);
+                $textoTraducido=traducir($textoEntrada,$idiomaEntrada,$idiomaSalida,$formal,$formato);
 
                 // Insercción en la base de datos
                 $insert = $conn->prepare("INSERT INTO traducciones (idioma_entrada, idioma_salida, texto, traduccion, num_caracteres, formal, formato) VALUES (?, ?, ?, ?, ?, ?, ?)");
@@ -159,5 +113,56 @@ function buscarClavePorPalabra($palabra, $array) {
     $clave = array_search($palabra, $array);
     return $clave !== false ? $clave : 'default';
 }
-?>
+
+function traducir($textoEntrada,$idiomaEntrada,$idiomaSalida,$formal,$formato){
+    // Datos para la solicitud
+    $apiKey = "bd7210a9-bcd9-df24-d8b8-01aec4676138:fx";
+
+    // Configuración de la solicitud cURL
+    $url = "https://api-free.deepl.com/v2/translate";
+    if($idiomaEntrada!='default'){
+        $data = array(
+            'text' => $textoEntrada,
+            'target_lang' => $idiomaSalida,
+            'source_lang' => $idiomaEntrada,
+            'formality' => $formal?'prefer_more':'default',
+            'preserve_formatting' => $formato
+        );
+    }else{
+        $data = array(
+            'text' => $textoEntrada,
+            'target_lang' => $idiomaSalida,
+            'formality' => $formal?'prefer_more':'default',
+            'preserve_formatting' => $formato
+        );
+    }  
+
+    $options = array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => http_build_query($data),
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: DeepL-Auth-Key ' . $apiKey,
+            'Content-Type: application/x-www-form-urlencoded',
+        ),
+    );
+
+    $ch = curl_init();
+    curl_setopt_array($ch, $options);
+
+    // Ejecutar la solicitud cURL y obtener la respuesta
+    $response = json_decode(curl_exec($ch),true);
+    $textoTraducido = $response['translations'][0]['text'];
+
+    // Verificar si hubo algún error
+    if (curl_errno($ch)) {
+        echo 'Error en la solicitud cURL: ' . curl_error($ch);
+    }
+
+    // Cerrar la sesión cURL
+    curl_close($ch);
+    
+    return $textoTraducido;
+}
 
